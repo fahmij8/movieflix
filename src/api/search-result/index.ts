41dfdb59783c1e3b4fb9b614bd323df383fc538e
fahmij8 <fahmijabbar12@gmail.com>
2022-11-28 06:32:21 +0700

    feat: add custom hooks for search result

---
+++ src/api/search-result/index.ts
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useAxios } from "hooks/";
+import { BASE_URL } from "constants/";
+import { useMovieflixContext } from "context/";
+
+interface UseSearchResultProps {
+  s: string;
+  y: string;
+  type: string;
+}
+
+export const useSearchResult = ({ s, y, type }: UseSearchResultProps) => {
+  const { dispatch } = useMovieflixContext();
+
+  const url = new URL(BASE_URL);
+  url.searchParams.append("s", s);
+  url.searchParams.append("y", y);
+  url.searchParams.append("type", type);
+  url.searchParams.append("apikey", process.env.REACT_APP_API_KEY ?? "");
+
+  const [loading, error, data] = useAxios(url.toString(), "GET");
+
+  useEffect(() => {
+    dispatch({ type: "LOADING_SEARCH_RESULT", payload: loading });
+    if (data) {
+      dispatch({ type: "SET_SEARCH_RESULT", payload: data });
+    }
+
+    if (error) {
+      dispatch({ type: "ERROR_SEARCH_RESULT", payload: error });
+    }
+  }, [loading]);
+};
