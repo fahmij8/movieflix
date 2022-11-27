398980bf4511bf0a6371d13ab41809ab159d8663
fahmij8 <fahmijabbar12@gmail.com>
2022-11-28 06:31:03 +0700

    feat: 75% home page

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,14 +1,37 @@
 import { HeaderHome } from "components/HeaderHome";
 import { Logo } from "components/Logo";
+import { Search } from "components/Search";
 import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
 
 const Home = () => {
+  const ResponsiveLogo = styled(Logo)(({ theme }) => ({
+    margin: "20px 0",
+    [theme.breakpoints.down("xs")]: {
+      width: "125px",
+      height: "50px"
+    },
+    [theme.breakpoints.up("sm")]: {
+      width: "200px",
+      height: "80px"
+    }
+  }));
+
   return (
     <main>
       <HeaderHome />
-      <Grid container direction="column">
-        <Logo />
-      </Grid>
+      <Container fixed>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <ResponsiveLogo />
+          <Search />
+        </Grid>
+      </Container>
     </main>
   );
 };
